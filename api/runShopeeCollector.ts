75709911cbc881e04051f87cc8b72cce3cdfa4ae
malaccoy <camargosx94@gmail.com>
2025-12-03 18:18:57 -0800

    sera?

--- api/runShopeeCollector.ts
+++ api/runShopeeCollector.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import * as crypto from 'crypto';
-import { saveMessageLog } from './_db';
+import { saveMessageLog } from './_db.js';
 
 // --- Types & Interfaces ---
 
